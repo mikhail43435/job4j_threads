c413cc321e9caa3b4824298388d890cbe18d05ce
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-05-25 19:30:38 +0300

    Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/threads/concurrent/ThreadState.java
@@ -0,0 +1,30 @@
+package ru.job4j.threads.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                    for (long i = 0; i < 10000000000L; i++) {
+                        i = i + 100 - 100;
+                    }
+                }
+        );
+        Thread secondThread = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        firstThread.start();
+        secondThread.start();
+        while (firstThread.getState() != Thread.State.TERMINATED
+                || secondThread.getState() != Thread.State.TERMINATED) {
+            System.out.print("\rFirst thread state: "
+                    + firstThread.getState()
+                    + "   Second thread state: "
+                    + secondThread.getState());
+        }
+        System.out.println();
+        System.out.println("All threads are terminated");
+    }
+}
