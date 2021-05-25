e7e67c63946afe3559413b9eda39fca759340a33
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-05-25 19:52:36 +0300

    Скачивание файла с ограничением. [#144271 #160654]

--- src/main/java/ru/job4j/threads/concurrent/filedownloader/WgetDF.java
+++ src/main/java/ru/job4j/threads/concurrent/filedownloader/WgetDF.java
@@ -18,27 +18,35 @@ public class WgetDF implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading stated at :" + LocalDateTime.now());
+        String fileName = "test_dl_" + url.substring(url.lastIndexOf("/") + 1);
+        System.out.println(fileName);
         try (var in = new BufferedInputStream(new URL(url).openStream());
-             var fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             var fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            long time1;
+            long time1 = System.currentTimeMillis();
             long time2;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                time1 = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 time2 = System.currentTimeMillis();
                 if ((time2 - time1) < speed) {
-                    Thread.sleep(speed - (time2 - time1));
+                    System.out.println(speed - (time2 - time1));
                 }
+                time1 = System.currentTimeMillis();
             }
-        } catch (InterruptedException | IOException e) {
+        } catch (IOException e) {
             Thread.currentThread().interrupt();
+            e.printStackTrace();
         }
         System.out.println("Downloading ended at :" + LocalDateTime.now());
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length < 2) {
+            System.out.println("Specify URL and speed parameters in start param");
+            System.out.println("Program terminated");
+            return;
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new WgetDF(url, speed));
