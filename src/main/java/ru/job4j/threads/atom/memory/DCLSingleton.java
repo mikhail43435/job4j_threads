c02c1399eca2fb97d1f3ae9ffa07a3b62f2efa86
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-07-07 19:31:39 +0300

    Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/threads/atom/memory/DCLSingleton.java
@@ -0,0 +1,20 @@
+package ru.job4j.threads.atom.memory;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    private DCLSingleton() {
+    }
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}
