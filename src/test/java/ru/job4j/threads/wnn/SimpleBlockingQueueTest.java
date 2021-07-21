175f2b83f169e7c54a1a23bebc7888fcc2526f83
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-07-21 13:19:23 +0300

    Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/threads/wnn/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/threads/wnn/SimpleBlockingQueueTest.java
@@ -5,64 +5,49 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 public class SimpleBlockingQueueTest {
-
     @Test
-    public void add() throws InterruptedException {
-        final int numOfOffers = 100;
-        final int queueSize = 10;
-        int[] maxValue = new int[1];
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(queueSize);
-        Thread threadProducer = new Thread(
-                () ->
-                {
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        Thread producer = new Thread(
+                () -> {
                     try {
-                        for (int i = 1; i <= numOfOffers; i++) {
-                            while (queue.isFull()) {
-                                Thread.sleep(10);
-                            }
+                        for (int i = 0; i <= 20; i++) {
                             queue.offer(i);
-                            System.out.println("добавил:" + i + "    размер:" + queue.getCurrentSize());
-                            maxValue[0] = Math.max(maxValue[0], queue.getCurrentSize());
+                            System.out.println(queue.getCurrentSize());
+                        }
 
-                        }
-                        while (!queue.isEmpty()) {
-                            Thread.sleep(1000);
-                        }
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("producer out");
-                }
+                         e.printStackTrace();
+                         Thread.currentThread().interrupt();
+                     }
 
-        );
-        Integer[] value = new Integer[2];
-        value[1] = 0;
-        Thread threadConsumer = new Thread(
-                () ->
-                {
-                    try {
-                        while (!queue.isFull()) {
-                            Thread.sleep(1000);
-                        }
-                        while (!threadProducer.isInterrupted()) {
-                            queue.poll();
-                            System.out.println("удалил. размер: " + queue.getCurrentSize());
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("consumer out");
                 }
         );
-        threadConsumer.start();
-        threadProducer.start();
-        threadConsumer.join();
-        while (!queue.isEmpty()) {
-            Thread.sleep(1000);
-        }
-        notifyAll();
-        threadConsumer.interrupt();
-        assertThat(maxValue[0], is(queueSize));
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                            System.out.println(">>> " + queue.getCurrentSize());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)));
     }
 }
