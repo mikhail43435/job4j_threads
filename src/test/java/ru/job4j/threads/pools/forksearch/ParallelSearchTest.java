a56b24116b4b21b48d14ea0ececfbf1b6fbecbf7
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-09-13 11:23:55 +0300

    ForkJoinPool [#315067 #202315]

--- src/test/java/ru/job4j/threads/pools/forksearch/ParallelSearchTest.java
+++ src/test/java/ru/job4j/threads/pools/forksearch/ParallelSearchTest.java
@@ -94,4 +94,15 @@ public class ParallelSearchTest {
         assertThat(ps.compute(), is(-1));
     }
 
+
+    @Test
+    public void test011ThenDigitsFoundInRangeAndMoreThan30ElementsInArray() {
+        Integer[] array = new Integer[]{
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+                35, 36, 37, 40, 45, 87, 58,584};
+        ParallelSearch ps = new ParallelSearch(array, 4, 26, 27);
+        assertThat(ps.compute(), is(26));
+    }
+
 }
