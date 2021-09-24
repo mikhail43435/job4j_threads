1c4c4519e6c404e30060843a856ac6f7ee9a1b3d
Mike <68198830+mikhail43435@users.noreply.github.com>
2021-09-24 22:02:27 +0300

    CompletableFuture [#361626]

--- src/main/java/ru/job4j/threads/completablefuture/rolcolsum/RolColSum.java
+++ src/main/java/ru/job4j/threads/completablefuture/rolcolsum/RolColSum.java
@@ -1,13 +1,19 @@
 package ru.job4j.threads.completablefuture.rolcolsum;
 
-import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 
 /**
+ * Задача:
  * Дан каркас класса.
  * Ваша задача подсчитать суммы по строкам и столбцам матрицы.
  * - sums[i].rowSum - сумма элементов по i строке
  * - sums[i].colSum  - сумма элементов по i столбцу
+ * Реализовать последовательную версию программы
+ * Реализовать асинхронную версию программы. i - я задача считает сумму по i столбцу и i строке
+ * Написать тесты
  */
 
 public class RolColSum {
@@ -47,31 +53,21 @@ public class RolColSum {
             this.rowSum = 0;
             this.colSum = 0;
         }
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
     }
 
+    /**
+     * Считает суммы по строкам и столбцам матрицы.
+     * - sums[i].rowSum - сумма элементов по i строке
+     * - sums[i].colSum  - сумма элементов по i столбцу
+     * @param matrix    - матрица
+     * @return          - массив объектов класса RolColSum
+     */
     public static Sums[] sum(int[][] matrix) {
         int tempSum = 0;
         Sums[] sums = new Sums[Math.max(matrix.length, matrix[0].length)];
         for (int i = 0; i < sums.length; i++) {
             sums[i] = new Sums();
         }
-
         // идем по строкам
         for (int i = 0; i < matrix.length; i++) {
             for (int k = 0; k < matrix[0].length; k++) {
@@ -88,13 +84,73 @@ public class RolColSum {
             sums[i].colSum = tempSum;
             tempSum = 0;
         }
-        System.out.println(Arrays.deepToString(sums));
         return sums;
     }
 
+    /**
+     * Считает суммы по строкам и столбцам матрицы
+     * в двух одтельных асинхронных потоках CompletableFuture
+     * по колонкам и срокам матрицы соответственно
+     * - sums[i].rowSum - сумма элементов по i строке
+     * - sums[i].colSum  - сумма элементов по i столбцу
+     * @param matrix    - матрица
+     * @return          - массив объектов класса RolColSum
+     */
     public static Sums[] asyncSum(int[][] matrix) {
-
-        return null;
+        Sums[] sums = new Sums[Math.max(matrix.length, matrix[0].length)];
+        for (int i = 0; i < sums.length; i++) {
+            sums[i] = new Sums();
+        }
+        CompletableFuture<Void> all = CompletableFuture.allOf(
+                countRowSum(matrix, sums), countColumnSum(matrix, sums)
+        );
+        while (!all.isDone()) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return sums;
     }
 
+    /**
+     * Метод возвращает экземплят класса CompletableFuture<Void>
+     * который выполняет подсчет сумм по строкам переданной матрицы
+     * и заносит их в переданный объект sums
+     * @param matrix    - матрица
+     * @param sums      - массив объектов класса RolColSum
+     * @return          - CompletableFuture<Void>
+     */
+    public static CompletableFuture<Void> countRowSum(int[][] matrix, Sums[] sums) {
+        return CompletableFuture.runAsync(
+                () -> {
+                    for (int i = 0; i < matrix.length; i++) {
+                        for (int k = 0; k < matrix[0].length; k++) {
+                            sums[i].rowSum += matrix[i][k];
+                        }
+                    }
+                }
+        );
+    }
+
+    /**
+     * Метод возвращает экземплят класса CompletableFuture<Void>
+     * который выполняет подсчет сумм по колонкам переданной матрицы
+     * и заносит их в переданный объект sums
+     * @param matrix    - матрица
+     * @param sums      - массив объектов класса RolColSum
+     * @return          - CompletableFuture<Void>
+     */
+        public static CompletableFuture<Void> countColumnSum(int[][] matrix, Sums[] sums) {
+        return CompletableFuture.runAsync(
+                () -> {
+                    for (int i = 0; i < matrix[0].length; i++) {
+                        for (int k = 0; k < matrix.length; k++) {
+                            sums[i].colSum += matrix[k][i];
+                        }
+                    }
+                }
+        );
+    }
 }
--- src/test/java/ru/job4j/threads/completablefuture/rolcolsum/RolColSumTest.java
+++ src/test/java/ru/job4j/threads/completablefuture/rolcolsum/RolColSumTest.java
@@ -1,50 +1,71 @@
 package ru.job4j.threads.completablefuture.rolcolsum;
 
-import junit.framework.TestCase;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class RolColSumTest {
 
     @Test
-    public void test01() {
+    public void testThenOneThread01() {
         int[][] matrix = new int[][]{{1, 2, 3}, {5, 2, 3}, {1, 2, 3}, {3, 4, 5}};
         RolColSum.Sums[] expected  = new RolColSum.Sums[4];
         expected [0] = new RolColSum.Sums(6, 10);
         expected [1] = new RolColSum.Sums(10, 10);
         expected [2] = new RolColSum.Sums(6, 14);
         expected [3] = new RolColSum.Sums(12, 0);
-        System.out.println(Arrays.deepToString(expected ));
-        assertArrayEquals(RolColSum.sum(matrix), expected );
+        assertArrayEquals(expected, RolColSum.sum(matrix) );
     }
 
-
     @Test
-    public void test02() {
+    public void testThenOneThread02() {
         int[][] matrix = new int[][]{{1, 2}, {3, 4}};
         RolColSum.Sums[] expected  = new RolColSum.Sums[2];
         expected [0] = new RolColSum.Sums(3, 4);
         expected [1] = new RolColSum.Sums(7, 6);
-        System.out.println(Arrays.deepToString(expected));
-        assertArrayEquals(RolColSum.sum(matrix), expected);
+        assertArrayEquals(expected, RolColSum.sum(matrix));
     }
 
 
     @Test
-    public void test03() {
+    public void testThenOneThread03() {
         int[][] matrix = new int[2][2];
         RolColSum.Sums[] expected  = new RolColSum.Sums[2];
         expected [0] = new RolColSum.Sums(0, 0);
         expected [1] = new RolColSum.Sums(0, 0);
-        System.out.println(Arrays.deepToString(expected));
-        assertArrayEquals(RolColSum.sum(matrix), expected);
+        assertArrayEquals(expected, RolColSum.sum(matrix));
+    }
+
+    @Test
+    public void testThenCompletableFutured01() {
+        int[][] matrix = new int[][]{{1, 2, 3}, {5, 2, 3}, {1, 2, 3}, {3, 4, 5}};
+        RolColSum.Sums[] expected  = new RolColSum.Sums[4];
+        expected [0] = new RolColSum.Sums(6, 10);
+        expected [1] = new RolColSum.Sums(10, 10);
+        expected [2] = new RolColSum.Sums(6, 14);
+        expected [3] = new RolColSum.Sums(12, 0);
+        assertArrayEquals(expected, RolColSum.asyncSum(matrix));
+    }
+
+
+    @Test
+    public void testThenCompletableFuture02() {
+        int[][] matrix = new int[][]{{1, 2}, {3, 4}};
+        RolColSum.Sums[] expected  = new RolColSum.Sums[2];
+        expected [0] = new RolColSum.Sums(3, 4);
+        expected [1] = new RolColSum.Sums(7, 6);
+        assertArrayEquals(expected, RolColSum.asyncSum(matrix));
+    }
+
+
+    @Test
+    public void testThenCompletableFuture03() {
+        int[][] matrix = new int[2][2];
+        RolColSum.Sums[] expected  = new RolColSum.Sums[2];
+        expected [0] = new RolColSum.Sums(0, 0);
+        expected [1] = new RolColSum.Sums(0, 0);
+        assertArrayEquals(expected, RolColSum.asyncSum(matrix));
     }
 
 }
